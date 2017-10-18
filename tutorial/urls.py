de42bc366257313af9de485e1d25d69a53c5bebd
lbias <306508354@qq.com>
2017-10-18 17:26:43 +0800

    add a schema

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework.schemas import get_schema_view
+
 from snippets import views
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^schema/$', schema_view),
 ]
